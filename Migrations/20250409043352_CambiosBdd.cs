acca55937a7329c9c70c5861f23550e4471af297
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2025-04-08 23:34:42 -0500

    Configuracion de la base de datos y vinculacion con la misma

---
+++ Migrations/20250409043352_CambiosBdd.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IngenieriaWeb1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiosBdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autos",
+                columns: table => new
+                {
+                    AutoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Placa = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Propietario = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Año = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autos", x => x.AutoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autos");
+        }
+    }
+}
